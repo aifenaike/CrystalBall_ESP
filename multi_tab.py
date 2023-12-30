9e30a199e711115c4e4cbfba88de5760fd08d1db
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2023-12-30 04:34:57 +0100

    Update multi_tab.py

--- multi_tab.py
+++ multi_tab.py
@@ -52,4 +52,5 @@ Callbacks.register_callbacks(app)
 
 # Running the server
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8050, use_reloader=True)
+    app.run_server(debug=False, host='0.0.0.0', port=8050, use_reloader=False)
+
